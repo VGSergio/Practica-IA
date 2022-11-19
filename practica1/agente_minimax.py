6270ddc247c1da6a7ee1e6e3ff593e579ce8abfc
VGSergio <svegasise@gmail.com>
2022-11-19 21:27:40 +0100

    Minimax

--- practica1/agente_minimax.py
+++ practica1/agente_minimax.py
@@ -6,48 +6,27 @@ from practica1.entorn import Direccio, AccionsRana, ClauPercepcio
 from practica1.agent import Estat
 
 
-def _get_players(estat: Estat):
-    ranas = estat[ClauPercepcio.POSICIO]
-    noms = []
-    for rana in ranas:
-        noms.append(rana)
-
-    return noms
-
-
 class RanaMiniMax(joc.Rana):
 
     def __init__(self, *args, **kwargs):
         super(RanaMiniMax, self).__init__(*args, **kwargs)
         self.__accions = None
 
-    def minimax(self, estat: Estat, turno_max: bool):
-        score = estat.evaluar()
-        if score != 0:
-            return score
-
-        puntuacio_fills = []
-        for estat_fill in estat.genera_fills(turno_max):
-            self.minimax(estat_fill, not turno_max)
-
-        if turno_max:
-            return max(puntuacio_fills)
-        else:
-            return min(puntuacio_fills)
+    def _minimax(self, estat: Estat):
+        pass
 
     def actua(
             self, percep: entorn.Percepcio
     ) -> entorn.Accio | tuple[entorn.Accio, object]:
-
         if self.esta_botant():
             return AccionsRana.ESPERAR
 
         estat = Estat(info=percep.to_dict())
 
         if self.__accions is None:
-            self.minimax(estat=estat, turno_max=True)
+            self._minimax(estat=estat)
 
-        if len(self.__accions) > 0:
+        if self.__accions and len(self.__accions) > 0:
             aux = self.__accions.pop()
             return aux[AccionsRana], aux[Direccio]
         else:
@@ -60,13 +39,14 @@ class Estat(Estat):
         if info is None:
             info = {}
 
-        self.__info = info | {AccionsRana: [], Direccio: []}
+        self.__info = info
         self.__pare = pare
 
         players = self[ClauPercepcio.POSICIO].keys()
         for player in players:
-            self[AccionsRana].append({player: AccionsRana.ESPERAR})
-            self[Direccio].append({player: None})
+            pos = self[ClauPercepcio.POSICIO][player]
+            self[player] = {AccionsRana: AccionsRana.ESPERAR, Direccio: None, ClauPercepcio.POSICIO: pos}
+        # self.__info.pop(ClauPercepcio.POSICIO)
 
     def genera_fills(self, turno_max) -> list:
         indice = 0 if turno_max else 1
@@ -77,15 +57,32 @@ class Estat(Estat):
                 for move in Direccio:
                     padre = copy.deepcopy(self)
                     nou_estat = Estat(info=padre.__info, pare=padre)
-                    player = _get_players(padre)[indice]
-                    nou_estat[AccionsRana][indice][player] = accio
-                    nou_estat[Direccio][indice][player] = move
-                    pos = nou_estat[ClauPercepcio.POSICIO][player]
-                    nou_estat[ClauPercepcio.POSICIO][player] = self.__sigiente_casilla(pos, accio, move)
+                    player = nou_estat.get_max()
+                    nou_estat[player][AccionsRana] = accio
+                    nou_estat[player][Direccio] = move
+                    pos = nou_estat[player][ClauPercepcio.POSICIO]
+                    nou_estat[player][ClauPercepcio.POSICIO] = self.__sigiente_casilla(pos, accio, move)
+                    nou_estat[ClauPercepcio.POSICIO][player] = nou_estat[player][ClauPercepcio.POSICIO]
                     if nou_estat.legal():
                         fills.append(nou_estat)
 
         return fills
+
+    def get_max(self) -> str:
+        players = self.__get_players()
+        return players[0]
+
+    def get_min(self) -> str:
+        players = self.__get_players()
+        return players[1]
+
+    def __get_players(self):
+        ranas = self[ClauPercepcio.POSICIO]
+        noms = []
+        for rana in ranas:
+            noms.append(rana)
+
+        return noms
 
     def __sigiente_casilla(self, actual, accio: AccionsRana, direccio: Direccio) -> tuple:
         if accio is AccionsRana.ESPERAR:
@@ -113,9 +110,11 @@ class Estat(Estat):
         posiciones = self[ClauPercepcio.POSICIO].values()
         return posiciones in self[ClauPercepcio.OLOR]
 
-    def evaluar(self):
+    def evaluar(self) -> int:
         if self.es_meta():
-            posiciones = self[ClauPercepcio.POSICIO].values()
-            return 1 if posiciones[0] is self[ClauPercepcio.OLOR] else -1
-        else:
-            return 0
+            player_max = self.get_max()
+            if self[player_max][ClauPercepcio.POSICIO] == self[ClauPercepcio.OLOR]:
+                return 1
+            else:
+                return -1
+        return 0
