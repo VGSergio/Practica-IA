c0fa24f21ef68aafbb61dc59ee72c230f9308510
VGSergio <svegasise@gmail.com>
2022-11-21 20:18:38 +0100

    Minimax

--- practica1/agente_minimax.py
+++ practica1/agente_minimax.py
@@ -1,46 +1,10 @@
 import copy
+from queue import PriorityQueue
 
 from ia_2022 import entorn
 from practica1 import joc
 from practica1.entorn import Direccio, AccionsRana, ClauPercepcio
 from practica1.agent import Estat
-
-
-def minimax(turno_max: bool, profundidad: int, estat: Estat):
-    if estat.es_meta() or profundidad == 0:
-        if estat.es_meta(): print("yoooooooooooooooooooooooooyuyuyu")
-        return estat, estat.evaluar()
-
-    if turno_max:
-        hijos = estat.genera_fills(turno_max)
-        acciones_hijos = []
-        puntos_hijos = []
-        for hijo in hijos:
-            accion, puntuacion = minimax(not turno_max, profundidad - 1, hijo)
-            acciones_hijos.append(accion)
-            puntos_hijos.append(puntuacion)
-        puntuacion_max = -2
-        indice_max = -1
-        for puntuacion in range(len(puntos_hijos)):
-            if puntos_hijos[puntuacion] > puntuacion_max:
-                puntuacion_max = puntos_hijos[puntuacion]
-                indice_max = puntuacion
-        return acciones_hijos[indice_max], puntos_hijos[indice_max]
-    else:
-        hijos = estat.genera_fills(turno_max)
-        acciones_hijos = []
-        puntos_hijos = []
-        for hijo in hijos:
-            accion, puntuacion = minimax(not turno_max, profundidad - 1, hijo)
-            acciones_hijos.append(accion)
-            puntos_hijos.append(puntuacion)
-        puntuacion_min = 2
-        indice_min = -1
-        for puntuacion in range(len(puntos_hijos)):
-            if puntos_hijos[puntuacion] < puntuacion_min:
-                puntuacion_min = puntos_hijos[puntuacion]
-                indice_min = puntuacion
-        return acciones_hijos[indice_min], puntos_hijos[indice_min]
 
 
 class RanaMiniMax(joc.Rana):
@@ -50,27 +14,76 @@ class RanaMiniMax(joc.Rana):
         self.__accions = None
         self.__tancats = set()
 
+    def minimax(self, turno_max: bool, profundidad: int, estat: Estat):
+        if estat in self.__tancats:
+            print("Repetido")
+            if turno_max:
+                return -2, estat
+            return 2, estat
+
+        self.__tancats.add(estat)
+
+        if estat.es_meta() or profundidad == 0:
+            if estat.es_meta():
+                print("yoooooooooooooooooooooooooyuyuyu")
+            return estat.evaluar(), estat
+
+        if turno_max:
+            hijos = estat.genera_fills(True)
+            acciones_hijos = []
+            puntos_hijos = []
+            for hijo in hijos:
+                padre = copy.deepcopy(self)
+                puntuacion, accion = padre.minimax(False, profundidad - 1, hijo)
+                puntos_hijos.append(puntuacion)
+                acciones_hijos.append(accion)
+            puntuacion_max = -2
+            indice_max = -1
+            for puntuacion in range(len(puntos_hijos)):
+                if puntos_hijos[puntuacion] > puntuacion_max:
+                    puntuacion_max = puntos_hijos[puntuacion]
+                    indice_max = puntuacion
+            return puntos_hijos[indice_max], acciones_hijos[indice_max]
+        else:
+            hijos = estat.genera_fills(False)
+            acciones_hijos = []
+            puntos_hijos = []
+            for hijo in hijos:
+                padre = copy.deepcopy(self)
+                puntuacion, accion = padre.minimax(True, profundidad - 1, hijo)
+                puntos_hijos.append(puntuacion)
+                acciones_hijos.append(accion)
+            puntuacion_min = 2
+            indice_min = -1
+            for puntuacion in range(len(puntos_hijos)):
+                if puntos_hijos[puntuacion] < puntuacion_min:
+                    puntuacion_min = puntos_hijos[puntuacion]
+                    indice_min = puntuacion
+            return puntos_hijos[indice_min], acciones_hijos[indice_min]
+
     def actua(
             self, percep: entorn.Percepcio
     ) -> entorn.Accio | tuple[entorn.Accio, object]:
         if self.esta_botant():
             return AccionsRana.ESPERAR
 
-        estat = Estat(info=percep.to_dict())
+        estat = Estat(info=percep.to_dict(), turno=self.nom)
 
-        if self.__accions is None:
-            aux = minimax(turno_max=True, profundidad=30, estat=estat)
-        print(aux)
-        if aux:
-            aux1 = aux[0]['Miquel']
-            return aux1[AccionsRana], aux1[Direccio]
+        turno_max = False
+        if estat.get_max() == self.nom:
+            turno_max = True
+
+        _, accion = self.minimax(turno_max=turno_max, profundidad=12, estat=estat)
+
+        if accion:
+            return accion[self.nom][AccionsRana], accion[self.nom][Direccio]
         else:
             return AccionsRana.ESPERAR
 
 
 class Estat(Estat):
 
-    def __init__(self, info: dict = None, pare=None):
+    def __init__(self, info: dict = None, pare=None, turno: str = ''):
         if info is None:
             info = {}
 
@@ -81,6 +94,7 @@ class Estat(Estat):
         for player in players:
             pos = self[ClauPercepcio.POSICIO][player]
             self[player] = {AccionsRana: AccionsRana.ESPERAR, Direccio: None, ClauPercepcio.POSICIO: pos}
+        self["Turno"] = turno
 
     def genera_fills(self, turno_max) -> list:
         fills = []
@@ -90,17 +104,29 @@ class Estat(Estat):
                 for move in Direccio:
                     padre = copy.deepcopy(self)
                     nou_estat = Estat(info=padre.__info, pare=padre)
-                    player = nou_estat.get_max()
-                    if not turno_max:
+                    if turno_max:
                         player = nou_estat.get_min()
+                    else:
+                        player = nou_estat.get_max()
                     nou_estat[player][AccionsRana] = accio
                     nou_estat[player][Direccio] = move
                     pos = nou_estat[player][ClauPercepcio.POSICIO]
-                    nou_estat[player][ClauPercepcio.POSICIO] = self.__sigiente_casilla(pos, accio, move)
+                    nou_estat[player][ClauPercepcio.POSICIO] = nou_estat.__sigiente_casilla(pos, accio, move)
                     nou_estat[ClauPercepcio.POSICIO][player] = nou_estat[player][ClauPercepcio.POSICIO]
+                    nou_estat["Turno"] = player
                     if nou_estat.legal():
                         fills.append(nou_estat)
-
+            else:
+                padre = copy.deepcopy(self)
+                nou_estat = Estat(info=padre.__info, pare=padre)
+                if turno_max:
+                    player = nou_estat.get_min()
+                else:
+                    player = nou_estat.get_max()
+                nou_estat[player][AccionsRana] = accio
+                nou_estat[player][Direccio] = None
+                nou_estat["Turno"] = player
+                fills.append(nou_estat)
         return fills
 
     def get_max(self) -> str:
@@ -153,3 +179,23 @@ class Estat(Estat):
             else:
                 return -1
         return 0
+
+    def legal(self) -> bool:
+        player = self["Turno"]
+        parets = self[ClauPercepcio.PARETS]
+        tam = self[ClauPercepcio.MIDA_TAULELL]
+        new_pos = self[player][ClauPercepcio.POSICIO]
+
+        new_pos = list(new_pos)
+
+        if (tam[0] > new_pos[0] >= 0) and (tam[1] > new_pos[1] >= 0):
+            if new_pos not in parets:
+                return True
+
+        return False
+
+    def __eq__(self, other):
+        return self.__info == other.__info
+
+    def __hash__(self):
+        return hash(tuple(self.__info))
